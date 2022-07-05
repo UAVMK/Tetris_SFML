119d4de5df01c8a860d611ef251764306660039f
Vladyslav Kharchenko <ytfir3fly@outlook.com>
2022-07-05 17:51:06 +0300

    almost final release

--- Tetris_SFML/main.cpp
+++ Tetris_SFML/main.cpp
@@ -1,14 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
 
-
-
 const int HEIGHT = 20; // Height of game field
-const int WIDTH = 10; // Width of game field
+const int WIDTH = 14; // Width of game field
 
-int field[HEIGHT][WIDTH] = { 0 }; // Game field
+int field[HEIGHT][WIDTH] = { 0 }; // Setup game field
 
-// Setup for tatramino figures
+// Setup of all Tetraminos
 int figures[7][4] =
 {
 	1,3,5,7, // I
@@ -25,148 +23,174 @@ struct Point
 	int x, y;
 } a[4], b[4];
 
+// Check if we leave a corner lines
 bool check()
 {
 	for (int i = 0; i < 4; i++)
-		if (a[i].x < 0 || a[i].x >= HEIGHT || a[i].y >= WIDTH) return 0;
+		if (a[i].x < 0 || a[i].x >= WIDTH || a[i].y >= HEIGHT) return 0;
 		else if (field[a[i].y][a[i].x]) return 0;
 
 	return 1;
-}
+};
+
 
 int main()
 {
-	srand(time(0)); // Random for tetraminos
+	srand(time(0));
 
-	sf::RenderWindow window(sf::VideoMode(320, 480), "Tetris");
+	// Create a window
+	sf::RenderWindow window(sf::VideoMode(320, 480), "FUCKIN' TETRIS <3");
 
-	// Creating and loading game tetramino texture
-	sf::Texture texture;
-	texture.loadFromFile("Y:\\IT\\C++\\Tetris_SFML\\tiles.png");
+	// All Textures
+	sf::Texture texture, texture_background, texture_frame;
+	texture.loadFromFile("Y:\\IT\\C++\\Tetris_SFML\\tiles.png"); // Texture for Tetramino
+	texture_background.loadFromFile("Dont forget to add!"); // Texture for background
+	texture_frame.loadFromFile("Dont forget to add!"); // Texture for frame
 
 	// Sprite creating
-	sf::Sprite sprite(texture);
+	sf::Sprite sprite(texture), sprite_background(texture_background), sprite_frame(texture_frame);
 
-	// Cutting 18õ18 pixels cube
+	// Creating a 18x18 pixels square
 	sprite.setTextureRect(sf::IntRect(0, 0, 18, 18));
 
-	// Setup Horizontal Movement, rotating and colors
-	bool rotate = 0;
-	int horizontalMovement = 0;
-	int colorNum = 1;
+	// Setup variables
+	int horizontalMovement = 0; // Variable for horizontal movement
+	bool rotate = 0; // Vairable for rotate
+	int colorNum = 1; // Variable for setups a colors
+	bool beginGame = true; // Variable for begin game
+	int n = rand() % 7;
 
-	// Setup timer and delay
+	// Variables for timer and delay
 	float timer = 0, delay = 0.3;
 
-	// Setup clock (timer)
+	// Clock / timer
 	sf::Clock clock;
 
 
 	// Main cycle of app
 	while (window.isOpen())
 	{
-		// Getting time from begin and convert it to seconds
+		// Setup, convert and restart timer
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
 		timer += time;
 
-		// Schedule of event in cycle
+		// Event schedule
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// Close window event
+			// Closing window
 			if (event.type == sf::Event::Closed)
 				window.close();
 
-			// Checking what key was pressed
+			// Check keyboard
 			if (event.type == sf::Event::KeyPressed)
-				// Rotate
-				if (event.key.code == sf::Keyboard::Up) rotate = true;
-			// Move to the left
-				else if (event.key.code == sf::Keyboard::Left) horizontalMovement = -1;
-			// Move to the right
-				else if (event.key.code == sf::Keyboard::Right) horizontalMovement = 1;
+				if (event.key.code == sf::Keyboard::Up) rotate = true; // Up button = Rotate
+				else if (event.key.code == sf::Keyboard::Left) horizontalMovement = -1; // Left button = Move to the left
+				else if (event.key.code == sf::Keyboard::Right) horizontalMovement = 1; // Right button = Move to the right
 		}
 
-		// Horizontal Movement
-		for (int i = 0; i < 4; i++)
-		{
-			b[i] = a[i];
-			a[i].x += horizontalMovement;
-		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) delay = 0.05; // Down button = Increase falling speed
+
+		for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += horizontalMovement; } // Horizontal movement
+
+		// Setup return point
+		if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+
 
 		// Rotate
 		if (rotate)
 		{
-			Point p = a[1]; // Setup center of rotating
+			// Setup a rotation point of tetramino
+			Point p = a[1];
 			for (int i = 0; i < 4; i++)
 			{
-				int x = a[i].y - p.y;
-				int y = a[i].x - p.x;
+				int x = a[i].y - p.y; //y - y0
+				int y = a[i].x - p.x; //x - x0
 				a[i].x = p.x - x;
 				a[i].y = p.y + y;
 			}
-
-			// Check lines after rotating, if go out return old position
-			if (!check())
-			{
-				for (int i = 0; i < 4; i++)
-					a[i] = b[i];
-			}
+			// Return point
+			if (!check()) { for (int i = 0; i < 4; i++) a[i] = b[i]; }
 
 		}
 
-		// Falling down tetraminos
+		// Down movement
 		if (timer > delay)
 		{
-			for (int i = 0; i < 4; i++)
-			{
-				b[i] = a[i]; a[i].y += 1;
-			}
+			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
 			if (!check())
 			{
 				for (int i = 0; i < 4; i++) field[b[i].y][b[i].x] = colorNum;
 				colorNum = 1 + rand() % 7;
-				int n = rand() % 7;
+				n = rand() % 7;
 				for (int i = 0; i < 4; i++)
 				{
-					a[i].x = figures[tetraminoType][i] % 2;
-					a[i].y = figures[tetraminoType][i] / 2;
+					a[i].x = figures[n][i] % 2;
+					a[i].y = figures[n][i] / 2;
 				}
+
 			}
 			timer = 0;
 
 		}
 
+		// Check vanishing line
+		int k = HEIGHT - 1;
+		for (int i = HEIGHT - 1; i > 0; i--)
+		{
+			int count = 0;
+			for (int j = 0; j < WIDTH; j++)
+			{
+				if (field[i][j]) count++;
+				field[k][j] = field[i][j];
+			}
+			if (count < WIDTH) k--;
+		}
 
-		// Setup tetramino type
-		int tetraminoType = 3;
-
-		// Appearence on the field
-		if (a[0].x == 0)
+		// Appearance on the field
+		if (beginGame)
+		{
+			beginGame = false;
+			n = rand() % 7;
 			for (int i = 0; i < 4; i++)
 			{
-				a[i].x = figures[tetraminoType][i] % 2;
-				a[i].y = figures[tetraminoType][i] / 2;
+				a[i].x = figures[n][i] % 2;
+				a[i].y = figures[n][i] / 2;
 			}
+		}
+		horizontalMovement = 0; rotate = 0; delay = 0.3;
 
-		horizontalMovement = 0;
-		rotate = 0;
 
-		// Setup background
-		window.clear(sf::Color::White);
+		// Drawing
+		window.clear(sf::Color::White); // Setup background color as white
+		window.draw(sprite_background);
 		for (int i = 0; i < HEIGHT; i++)
-
+			for (int j = 0; j < WIDTH; j++)
+			{
+				if (field[i][j] == 0) continue;
+				sprite.setTextureRect(sf::IntRect(field[i][j] * 18, 0, 18, 18));
+				sprite.setPosition(j * 18, i * 18);
+				sprite.move(28, 31); // ---> OFFSET POSITION <---
+				window.draw(sprite);
+			}
 
 		for (int i = 0; i < 4; i++)
 		{
-			// Setup position for eact tetramino piece
-			sprite.setPosition(a[i].x * 18, a[i].y * 18);
+			// Setup tetramino colors
+			sprite.setTextureRect(sf::IntRect(colorNum * 18, 0, 18, 18));
 
-			// Sprite drawing
+			// Making a new position
+			sprite.setPosition(a[i].x * 18, a[i].y * 18);
+			sprite.move(28, 31); // ---> OFFSET <---
+
+			// Sprite draw
 			window.draw(sprite);
 		}
-		// Window drawing
+		// Frame draw
+		window.draw(sprite_frame);
+
+		// Window draw
 		window.display();
 	}
 
